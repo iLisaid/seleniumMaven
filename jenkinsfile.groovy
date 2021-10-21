0472ecbbe17bca4e545a1176c7247dfd5c8716e0
ishulgin <ivan.shulgin@artezio.com>
2021-10-21 16:32:48 +0300

    Init jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,25 +1,20 @@
 node {
     env.IMAGE_NAME = "selenium"
     env.TAG_NAME = "local"
-    env.REMOTE_GIT_URL = "https://$GIT_USER:$GIT_PASSWD@git.artezio.net/ndokutovich/art-hr.git"
+    env.REMOTE_GIT_URL = "https://github.com/iLisaid/seleniumMaven.git"
 
     stage("Cloning Git") {
-        git changelog: false, branch: "dev", credentialsId: "art-hr-service", url: REMOTE_GIT_URL
+        git changelog: false, branch: "master", url: REMOTE_GIT_URL
     }
 
     stage("Building image") {
-        dir("gatling-build") {
-            sh '''
-                cp -R ../src/main/docker/gatling/. .
-                cp -R ../src/gatling/ .
-                docker build -t $IMAGE_NAME:$TAG_NAME .
+        sh '''
+                docker build -t $IMAGE_NAME:$TAG_NAME ./seleniumMaven
             '''
-            deleteDir()
-        }
     }
 
     stage("Run Gatling") {
-        sh "docker run --rm -v $WORKSPACE/gatling-results:/opt/gatling/results -e JAVA_OPTS='-Dusers=$USERS -Dduration=$DURATION -DserverUrl=$SERVER_URL -DkeycloakServerUrl=$KEYCLOAK_SERVER_URL -DkeycloakRealm=$KEYCLOAK_REALM -DkeycloakUser=$KEYCLOAK_USER -DkeycloakPassword=$KEYCLOAK_PASSWORD' -e SIMULATIONS=$SIMULATIONS $IMAGE_NAME:$TAG_NAME"
+        sh "docker run --rm $IMAGE_NAME:$TAG_NAME"
     }
 
     stage("Cleaning up") {
