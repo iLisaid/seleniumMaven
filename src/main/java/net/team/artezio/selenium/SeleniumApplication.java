3b92404031eebd0714c16198208615f55044fa74
ishulgin <ivan.shulgin@artezio.com>
2021-10-21 17:53:27 +0300

    Update for linux

--- src/main/java/net/team/artezio/selenium/SeleniumApplication.java
+++
@@ -1,9 +0,0 @@
-package net.team.artezio.selenium;
-
-public class SeleniumApplication {
-
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/test/java/net/team/artezio/selenium/SeleniumApplicationTests.java
+++ src/test/java/net/team/artezio/selenium/SeleniumApplicationTests.java
@@ -1,28 +1,47 @@
 package net.team.artezio.selenium;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class SeleniumApplicationTests {
-    WebDriver driver;
 
+    WebDriver driver;
+    static final String APP_URL = "https://artteam-pe-team-qa.oo-aps.artezio.net";
+    static final String HOST_URL = "http://localhost:4444/wd/hub";
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions opt = new ChromeOptions();
+        try {
+            driver = new RemoteWebDriver(new URL(HOST_URL), opt);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(APP_URL);
     }
 
     @Test
-    public void helloWorldTest() {
-        driver.get("https://artteam-pe-team-qa.oo-aps.artezio.net");
+    public void assertTitle(){
+        String expectedTitle = "Log in to Keycloak";
+        assertEquals(driver.getTitle(), expectedTitle);
     }
 
     @After
-    public void close(){
-        driver.close();
+    public void tearDown(){
+        if(driver != null) {
+            driver.quit();
+        }
     }
 }
-
